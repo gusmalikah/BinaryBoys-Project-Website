b650435bd26f6e6a98488121644d4d1aa0039607
unknown <hannanmalik1999@gmail.com>
2022-12-22 18:40:08 +0100

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../index.css"
+
+function Header(){
+    return(
+        <header>
+            <h1>Binary Boys</h1>
+        </header>
+    )
+}
+export default Header

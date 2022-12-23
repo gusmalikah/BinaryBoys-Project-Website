e1889fe3bea60074e6af3dc90d75b69e522022d9
unknown <hannanmalik1999@gmail.com>
2022-12-23 01:57:25 +0100

    bug fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,16 @@ function Hero(){
     return(
         <main>
             <div className="info">
-                <h1>About Project:</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt augue non 
-                    sapien dignissim tincidunt. Duis a maximus nunc. Donec sed elit nec orci interdum eleifend quis a arcu.
-                    Vivamus laoreet ultricies nibh sit amet efficitur. Cras vitae lacinia sem. Aenean a pharetra lectus, 
-                    sit amet vulputate urna. Praesent feugiat congue quam, vitae mattis diam. Donec ac libero et mi suscipit posuere.
-                    Sed lacinia massa a fermentum maximus.Integer sed enim eu elit hendrerit tempor. Suspendisse sit scelerisque.
-                    Mauris a turpis sapien.  Integer sed enim eu elit hendrerit tempor. Suspendisse sit amet diam eget nisi 
-                    mollis faucibus. Sed viverra tincidunt mattis. Maecenas a ligula fermentum, sollicitudin est sit amet, 
-                    laoreet erat. Etiam cursus sollicitudin eros eu scelerisque. Mauris a turpis sapien.
+                <h1>About Project</h1>
+                <p>
+                Binary Boys is the name of the student group for the course Software Innovation (DIT827) at University Of Gothenburg.
+                Binary Boys works in cooperation with Sahlgrenska University Hospital. It is a software that will connect
+                to the monitor (Moberg Monitor) used in ICU and read the files at the same time as they are written.
+                The outcome should be a visualization of the signals in the real time. The scope of this project reflects on 
+                understanding of binary data extraction, conversion then visualizing it with the help of MATLAB For this purpose 
+                the group decided to use Phyton as a programming language as according to our understanding Phyton would be the 
+                best for such a project within this scope.
+                <span>...Due to Data privacy the project repository is private...</span>
                 </p>
             </div>
             <div className="slide">
